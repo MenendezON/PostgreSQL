02b561edb492da14d3e73fc8201b426044f267bb
MenendezON <menendezon@gmail.com>
2023-09-18 22:08:29 +0000

    Modify and add some queries

--- queries.sql
+++ queries.sql
@@ -23,3 +23,75 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 
 /*Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)*/
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+/*---------------------*/
+
+BEGIN;
+
+update animals set species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+BEGIN;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species = '';
+
+SELECT * FROM animals;
+
+COMMIT;
+
+/*---------------------*/
+
+BEGIN;
+
+SELECT * FROM animals;
+
+DELETE FROM animals;
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+/*---------------------*/
+
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT BORNAFTERJAN0122;
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO BORNAFTERJAN0122;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
+
+SELECT * FROM animals;
+
+/*---------------------*/
+
+select count(*) from animals;
+
+select count(*) from animals where escape_attempts = 0;
+
+select avg(weight_kg) from animals;
+
+select max(weight_kg), min(weight_kg)
+
+SELECT neutered, COUNT(*) AS escape_count FROM animals WHERE escape_attempts = 1 GROUP BY neutered ORDER BY escape_count DESC;
+
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts FROM (SELECT species, COUNT(*) AS escape_attempts FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species) AS subquery GROUP BY species;
+
+
